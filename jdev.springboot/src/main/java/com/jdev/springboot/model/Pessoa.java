0b7312e63ea38c3b1cf45b1cf2d42c73fa29cc1c
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2024-11-25 11:46:46 -0300

    34.15 - Relacionamento um para muitos entre entidades

--- jdev.springboot/src/main/java/com/jdev/springboot/model/Pessoa.java
+++ jdev.springboot/src/main/java/com/jdev/springboot/model/Pessoa.java
@@ -1,11 +1,13 @@
 package com.jdev.springboot.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Pessoa implements Serializable {
@@ -21,6 +23,17 @@ public class Pessoa implements Serializable {
 	private String sobrenome;
 
 	private int idade;
+
+	@OneToMany(mappedBy = "pessoa")
+	private List<Telefone> telefones;
+
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
+	}
 
 	public int getIdade() {
 		return idade;
