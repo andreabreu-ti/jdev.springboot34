0f3d6c2591a0b433617f5f32f2a4bb352b912b32
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2024-12-03 17:58:54 -0300

    34.24 - Spring Security + JPA + DB - Validando Usuário-Modificado Versao

---
+++ jdev.springboot/src/main/java/com/jdev/springboot/model/Usuario.java
@@ -0,0 +1,79 @@
+package com.jdev.springboot.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String login;
+	private String senha;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return login;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
--- jdev.springboot/src/main/java/com/jdev/springboot/security/WebConfigSecurity.java
+++ jdev.springboot/src/main/java/com/jdev/springboot/security/WebConfigSecurity.java
@@ -1,22 +1,22 @@
 package com.jdev.springboot.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebConfigSecurity {
+
+    @Autowired
+    private ImplementacaoUserDetailsService implementacaoUserDetailsService;
 
     // Configuração do SecurityFilterChain para proteger URLs
     @Bean
@@ -37,40 +37,24 @@ public class WebConfigSecurity {
         return http.build();
     }
 
-    // Define o UserDetailsService com um usuário em memória
-    @Bean
-    public UserDetailsService userDetailsService() {
-        
-    	var userDetailsManager = new InMemoryUserDetailsManager();
-
-        // Cria um usuário em memória
-        userDetailsManager.createUser(
-            User.withUsername("andre")
-                .password("$2a$10$0.2/FfTOiLeYkZRgYDOJDOmR1/s0EL/SQjOsGktzPEHb2WpJkB7S2") // Senha em texto puro (somente para testes)
-                .roles("ADMIN")
-                .build()
-        );
-
-        return userDetailsManager;
-    }
-
-    // Define um PasswordEncoder (sem codificação, apenas para testes)
-    /*@Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }*/
-    
     // Define o PasswordEncoder para validar senhas criptografadas
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
+    // Configura o AuthenticationProvider com o UserDetailsService do banco de dados
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(implementacaoUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
 
     // Define o AuthenticationManager para ser usado no sistema
     @Bean
-    public AuthenticationManager authenticationManager(
-        AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 }
