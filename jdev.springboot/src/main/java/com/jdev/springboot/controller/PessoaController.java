2159bf6fee355d9ee66220da75b5e372114216cf
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2024-12-06 17:20:32 -0300

    34.31 - Resolvendo problemas de Atualizar o cadastro de Pessoa/Usuário

--- jdev.springboot/src/main/java/com/jdev/springboot/controller/PessoaController.java
+++ jdev.springboot/src/main/java/com/jdev/springboot/controller/PessoaController.java
@@ -51,6 +51,8 @@ public class PessoaController {
 //	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
 	public ModelAndView salvar(@Valid Pessoa pessoa, BindingResult bindingResult) {
 
+		pessoa.setTelefones(telefoneRepository.getTelefones(pessoa.getId()));
+		
 		if (bindingResult.hasErrors()) {
 			
 			ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
