14234ffa432f631c99cad78e821bcf31f4112722
andre_abreu <andre_abreu@04B-IN001606.agropalma.net>
2024-12-06 12:55:54 -0300

    34.29 - Adicionando Materialize usando CDN

--- jdev.springboot/src/main/java/com/jdev/springboot/Application.java
+++ jdev.springboot/src/main/java/com/jdev/springboot/Application.java
@@ -2,19 +2,29 @@ package com.jdev.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Application {
+public class Application implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(Application.class, args);
-		
+
 		/*
-		 * BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		 * String result = encoder.encode("123");
-		 * System.out.println(result);
-		 * */
+		 * BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(); String result =
+		 * encoder.encode("123"); System.out.println(result);
+		 */
+
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+
+		registry.addViewController("/login").setViewName("/login");
+		registry.setOrder(Ordered.LOWEST_PRECEDENCE);
 	}
 
 }
